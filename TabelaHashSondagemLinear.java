f8b871f20a06f19e9f7017d44939990f225348e7
Alana Queiroz <166722191+alanaqroz@users.noreply.github.com>
2025-10-14 22:16:58 -0300

    Update TabelaHashSondagemLinear.java
    Sondagem Linear: Adição de contador de colisões e correção de índice negativo no rehashing.

--- TabelaHashSondagemLinear.java
+++ TabelaHashSondagemLinear.java
@@ -4,12 +4,20 @@ public class TabelaHashSondagemLinear {
     private String[] chaves;
     private int[] valores;
     private int tamanho;
+    private long colisoes = 0;
 
     public TabelaHashSondagemLinear(int tamanho) {
         this.tamanho = tamanho;
         this.chaves = new String[tamanho];
         this.valores = new int[tamanho];
+        
     }
+
+    // retornar o número de colisões
+    public long getColisoes() {
+        return colisoes;
+    }
+    
     // Inserir chave e valor
     public void inserir(String chave, int valor) {
         int hash = FuncoesHash.hashPolinomial(chave, tamanho);
@@ -19,6 +27,37 @@ public class TabelaHashSondagemLinear {
         // Rehashing linear: (hash + j) % tamanho
         while (chaves[i] != null && !chaves[i].equals(chave)) {
             i = (hash + ++passo) % tamanho;
+        }
+
+        chaves[i] = chave;
+        valores[i] = valor;
+    }
+
+        // inserção para testes de desempenho
+    public void inserir(Registro registro, int tipoFuncaoHash) {
+        String chave = registro.getCodigoRegistro();
+        int valor = registro.getValor();
+
+        // Usa o método centralizado para obter o hash
+        int hash = FuncoesHash.obterHash(chave, tamanho, tipoFuncaoHash);
+        int i = hash;
+        int passo = 0;
+
+        while (chaves[i] != null && !chaves[i].equals(chave)) {
+            passo++;
+
+            int novoIndice = (hash + passo) % tamanho;
+
+            if (novoIndice < 0) {
+                novoIndice += tamanho;
+            }
+            i = novoIndice;
+
+            colisoes++; // Colisão: contador incrementado a cada sondagem
+
+            if (passo >= tamanho) {
+                return;
+            }
         }
 
         chaves[i] = chave;
